ec05af5f275cc0af181c2d160d690807843cadcb
Saffi Khawaja <saffikhawaja@gmail.com>
2026-01-22 14:13:13 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+System.Console.WriteLine("Welcome to the C# programming world!");
+System.Console.WriteLine("This is a simple console application.");
