5a859a2d87fd7312541706bf14f0bfada728f58c
Saffi Khawaja <saffikhawaja@gmail.com>
2026-01-22 16:04:58 +0500

    Smart input validator CLI project

---
+++ 01_SmartInputValidator/Program.cs
@@ -0,0 +1,24 @@
+﻿System.Console.WriteLine("Please input your name");
+string? name = Console.ReadLine();
+
+//checking for not null or whitespace
+if (string.IsNullOrWhiteSpace(name)) //learnt this through chatpgt 
+{
+    System.Console.WriteLine("do not input null please");
+}
+
+System.Console.WriteLine("Please input your age");
+int? age = int.Parse(Console.ReadLine());
+
+if (age is null)
+{
+    System.Console.WriteLine("do not enter null value for your age bruh");
+}
+
+//checking for a real age entered 
+if ((age < 0) || (age > 120))
+{
+    System.Console.WriteLine($"invalid age entered:{age} is not a real age dude ");
+}
+
+System.Console.WriteLine($"welcome home {name}, of age {age}");
